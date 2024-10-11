b75b53429ef6bff7ea1fbf44da3d4d01ebeb5f88
FabriciaJanuario <student.fabriciajanuario@gmail.com>
2024-10-11 14:24:49 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Rayssa Leal",
+        descricao: "A jovem skatista brasileira, conhecida como a fadinha do skate, se destaca por suas habilidades e conquistas no skate mundial. ",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada"
+    },
+    {
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz é uma judoca brasileira que se destaca no cenário internacional, representando o Brasil em grandes competições de judô.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judo judoca"
+    },
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Medalhista de ouro nas Olimpíadas, Rebeca é uma ginasta brasileira que fez história no esporte, sendo um exemplo de talento e dedicação.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginastica ouro"
+    }
+]
